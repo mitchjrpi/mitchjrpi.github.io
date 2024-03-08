ed699bdc0e01f25a9f590c91b6830018a74627c4
John Mitchell <johnmitchell@Johns-MacBook-Pro.local>
2024-03-08 17:14:54 -0500

    Initial commit

---
+++ generators/realign/map/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 98.1p5 (May 15th, 1998)
+# Associate internals original text with physical files.
+
+
+$key = q/map_int/;
+$ref_files{$key} = "$dir".q|map.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ generators/realign/map/labels.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 98.1p5 (May 15th, 1998)
+# Associate labels original text with physical files.
+
+
+$key = q/map_int/;
+$external_labels{$key} = "$URL/" . q|map.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
