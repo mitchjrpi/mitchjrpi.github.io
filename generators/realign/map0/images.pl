3cf80765ff7cebd7e68776df2b44ce7fa27d964a
John Mitchell <johnmitchell@Johns-MacBook-Pro.local>
2024-03-08 17:10:37 -0500

    Initial commit

--- generators/realign/map0/images.pl
+++
@@ -1,9 +0,0 @@
-# LaTeX2HTML 98.1p5 (May 15th, 1998)
-# Associate images original text with physical files.
-
-
-$key = q/{picture}(450,620)put(20,610)todayput(300,610)Code:put(310,590)line(1,0)25put(34LOPE,OFFSET:2829120016.50put(3,220)textcolorblueline(0,1)120{picture};LFS=12;AAT/;
-$cached_env_img{$key} = q|1#1|; 
-
-1;
-
--- generators/realign/map0/labels.pl
+++
@@ -1,6 +0,0 @@
-# LaTeX2HTML 98.1p5 (May 15th, 1998)
-# Associate labels original text with physical files.
-
-
-1;
-
